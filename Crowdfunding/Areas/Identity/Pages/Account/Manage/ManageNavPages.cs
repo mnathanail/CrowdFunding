74d2aea655d4c2c307aa68481c897bb874f3b569
AM592 <hrlagana@gmail.com>
2018-06-25 19:40:34 +0300

    minor change

--- Crowdfunding/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Crowdfunding/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -22,6 +22,8 @@ namespace Crowdfunding.Areas.Identity.Pages.Account.Manage
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string BackerHistory => "BackerHistory";
+
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
@@ -35,7 +37,9 @@ namespace Crowdfunding.Areas.Identity.Pages.Account.Manage
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
-    
+
+        public static string BackerHistoryNavClass(ViewContext viewContext) => PageNavClass(viewContext, BackerHistory);
+
         public static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
