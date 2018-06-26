1ca3dd1bc5b50ff091e05b979e9f392e4a2913c8
AM592 <hrlagana@gmail.com>
2018-06-26 13:43:27 +0300

    My projects table in Dashboard

--- Crowdfunding/Controllers/UserController.cs
+++ Crowdfunding/Controllers/UserController.cs
@@ -24,140 +24,21 @@ namespace Crowdfunding.Controllers
         // GET: User
         public async Task<IActionResult> Index()
         {
-            var crowdfundingContext = _context.AspNetUsers.Include(u => u.Project).Include(ub => ub.UsersBenefits);
-            return View(await crowdfundingContext.ToListAsync());
-        }
-
-        // GET: User/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
+            var userId = _GetPersonId();
+            if (userId == null)
             {
                 return NotFound();
             }
+            var usercontext = await _context.Project
+                .Include(u => u.User)
+                .Where(p => p.UserId == userId).ToListAsync();
 
-            var aspNetUsers = await _context.AspNetUsers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (aspNetUsers == null)
-            {
-                return NotFound();
-            }
-
-            return View(aspNetUsers);
+            return View(usercontext);
         }
-
-        // GET: User/Create
-        public IActionResult Create()
+        
+        private string _GetPersonId()
         {
-            ViewData["BenefitId"] = new SelectList(_context.Benefit, "BenefitId", "BenefitDesciption");
-            ViewData["UserId"] = new SelectList(_context.AspNetUsers, "Id", "Id");
-            return View();
-        }
-
-        // POST: User/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AspNetUsers aspNetUsers)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(aspNetUsers);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(aspNetUsers);
-        }
-
-        // GET: User/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var aspNetUsers = await _context.AspNetUsers.FindAsync(id);
-            if (aspNetUsers == null)
-            {
-                return NotFound();
-            }
-            return View(aspNetUsers);
-        }
-
-        // POST: User/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AspNetUsers aspNetUsers)
-        {
-            if (id != aspNetUsers.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(aspNetUsers);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AspNetUsersExists(aspNetUsers.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(aspNetUsers);
-        }
-
-        // GET: User/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var aspNetUsers = await _context.AspNetUsers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (aspNetUsers == null)
-            {
-                return NotFound();
-            }
-
-            return View(aspNetUsers);
-        }
-
-        // POST: User/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var aspNetUsers = await _context.AspNetUsers.FindAsync(id);
-            _context.AspNetUsers.Remove(aspNetUsers);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool AspNetUsersExists(string id)
-        {
-            return _context.AspNetUsers.Any(e => e.Id == id);
-        }
-
-        private bool UsersBenefitsExists(string id)
-        {
-            return _context.UsersBenefits.Any(e => e.UserId == id);
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
         }
     }
 }
